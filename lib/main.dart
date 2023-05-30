d150b4b9e9f6f6af0e727d3bc4ccaa73f6523b44
miyamoto <zeal20410147@fsgcl.jp>
2023-05-30 14:44:28 +0900

    ちょっとすすんだやつ

--- lib/main.dart
+++ lib/main.dart
@@ -44,10 +44,8 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          SearchBarWidget(),
-        ),
+      body: Column(
+        SearchBarWidget(),
       ),
     );
   }
--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -8,38 +8,26 @@ class SearchBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: '曲名 又は アーティスト名 を入力',
-            prefixIcon: Icon(Icons.search),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: '検索キーワードを入力してください',
+              ),
+            ),
           ),
-        ),
-        SizedBox(
-          //フィールドとボタンの間にスペースを作成
-          TextButton(
+          SizedBox(height: 20.0),
+          ElevatedButton(
             onPressed: () {
-              //検索ボタンが押された時の処理
-              //検索ロジックを追加する
+              // 検索ボタンが押されたときの処理
+              // ここに検索のロジックを追加してください
             },
             child: Text('検索'),
           ),
-        ),
+        ],
       ),
     );
-
-    /*
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-      child: Container(
-        height: 400,
-        color: const Color.fromARGB(255, 255, 0, 25),
-        child: const Center(
-          child: Text('メイン画像'),
-        ),
-      ),
-    );
-  }
-  */
   }
 }
