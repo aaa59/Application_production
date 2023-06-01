4c4342cce56038bd84d3a854ad83006f2db18f86
miyamoto <zeal20410147@fsgcl.jp>
2023-06-01 15:07:58 +0900

    新規登録ページを作れた

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'NewRegistrationPage.dart';
 //import 'FirstPage.dart';
 
 class HomePage extends StatelessWidget {
---
+++ lib/NewRegistrationPage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class NewRegistrationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('新規登録'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: '曲名',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'アーティスト名',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: '点数',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 32.0),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white, // 背景色を白に設定
+                    ),
+                    onPressed: () {
+                      // キャンセルボタンが押された時の処理
+                    },
+                    child: Text(
+                      'キャンセル',
+                      style: TextStyle(color: Colors.black), // テキストの色を黒に設定
+                    ),
+                  ),
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 登録ボタンが押された時の処理
+                    },
+                    child: Text('登録'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
