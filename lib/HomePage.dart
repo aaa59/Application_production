b3ff14c28b9ec1349a0ddd2476263b36b11e8293
miyamoto <zeal20410147@fsgcl.jp>
2023-06-01 14:46:47 +0900

    トップページ(HomePage)がある程度できた

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -34,6 +34,26 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(height: 20.0), // テキストとの間に余白を設定
+            Text(
+              '一覧',
+              style: TextStyle(fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20.0), // テキストとボタンの間に余白を設定
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(width: 30.0), // ボタンの左側に余白を設定
+                ElevatedButton(
+                  onPressed: () {
+                    // 新規登録ボタンが押された時の処理
+                    // ここに新規登録のロジックを書く
+                  },
+                  child: Text('新規登録'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
