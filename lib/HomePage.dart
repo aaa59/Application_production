6c1cf212b2fc4867d43061787b7c7a088e085c19
miyamoto <zeal20410147@fsgcl.jp>
2023-06-01 10:56:52 +0900

    検索ボタンにアイコンを実装した

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,30 +4,73 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("点数リスト"),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: '検索キーワードを入力してください',
-              ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('点数確認画面'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: '曲名 か アーティスト名を入力',
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // 検索ボタンが押された時の処理
+                    // ここに検索のロジックを書く
+                  },
+                  icon: Icon(Icons.search),
+                  label: Text('検索'),
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: () {
-              // 検索ボタンが押されたときの処理
-              // ここに検索のロジックを追加してください
-            },
-            child: Text('検索'),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+    /*
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('点数確認画面'),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '曲名 か アーティスト名を入力',
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // 検索ボタンが押された時の処理
+                  // ここに検索のロジックを書く
+                },
+                child: Text('検索'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    */
   }
 }
--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -4,7 +4,39 @@ class SearchBarWidget extends StatelessWidget {
   const SearchBarWidget({
     super.key,
   });
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('点数確認画面'),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '曲名 か アーティスト名を入力',
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // 検索ボタンが押された時の処理
+                  // ここに検索のロジックを書く
+                },
+                child: Text('検索'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,5 +61,6 @@ class SearchBarWidget extends StatelessWidget {
         ],
       ),
     );
+    */
   }
 }
